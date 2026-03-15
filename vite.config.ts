9086630c543ebb1f574d81eeaf3174b19012242d
lordcongra <lordcongra@gmail.com>
2026-03-15 15:01:27 -0400

    404 fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: 'poke-e-role',
+  base: '/poke-e-role/',
   server: {
     cors: {
       origin: "https://www.owlbear.rodeo",
